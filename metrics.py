756ac98ed04475bcc9b44ad40d8e38af85a3c151
Alex Yang <alyx.yang@gmail.com>
2015-05-30 19:21:41 -0700

    added code to import json and split into reviews, businesses, and users

---
+++ metrics.py
@@ -0,0 +1,33 @@
+import json
+
+def importJSON(dataset):
+    yelp_data = open(dataset)
+    r = []
+    b = []
+    u = []
+    for line in yelp_data:
+        try:
+            data = json.loads(line)
+        except ValueError:
+            print "Oops!"
+
+        if data["type"] == "user":
+            u.append(data)
+        elif data["type"] == "business":
+            b.append(data)
+        elif data["type"] == "review":
+            r.append(data)
+    return r, b, u
+
+if __name__ == "__main__":
+    dataset = "yelp_academic_dataset.json"
+    print "Loading data..."
+    r, b, u = importJSON(dataset)
+    print "Done."
+
+    print "# of reviews", len(r)
+    print "# of businesses", len(b)
+    print "# of users", len(u)
+
+
+
