a03c42a20e367d414a4bc85f5f751e6585cfc702
Alex Yang <alyx.yang@gmail.com>
2015-05-31 18:36:17 -0700

    refactored code to predict topics

--- analyze_review_topics.py
+++ analyze_review_topics.py
@@ -38,13 +38,18 @@ def find_topics_in_review(cleaned_text):
     return sorted_percentage
 
 
-if __name__ == "__main__":
-    vallartas_review = "This is my go-to Mexican place. First off, it has a drive through so my lazy butt doesn't have to change out of pajamas if I'm having a craving for a Cali Burrito. Fast service, good quality and good prices. Their tacos are also really good! I got the carne aside taco combo plate (with rice and beans) and loved it. Also if you order the chips and quac they give you a huge box full of hot crispy tortilla chips smothered in fresh guacamole. Big enough to share between at least 3-4 people, I can never finish it all because the portion is so big."
-
-    cleaned_text = clean_review(vallartas_review)
+def predict_topics(review):
+    cleaned_text = clean_review(review)
     sorted_topics = find_topics_in_review(cleaned_text)
 
     for percentage, topic_number in sorted_topics:
         #print lda.print_topic(topic_number)
         print str(percentage * 100) + "%" + "\t" + lda.print_topic(topic_number)
+
+if __name__ == "__main__":
+
+    print "Vallartas Review"
+    vallartas_review = "This is my go-to Mexican place. First off, it has a drive through so my lazy butt doesn't have to change out of pajamas if I'm having a craving for a Cali Burrito. Fast service, good quality and good prices. Their tacos are also really good! I got the carne aside taco combo plate (with rice and beans) and loved it. Also if you order the chips and quac they give you a huge box full of hot crispy tortilla chips smothered in fresh guacamole. Big enough to share between at least 3-4 people, I can never finish it all because the portion is so big."
+
+    predict_topics(vallartas_review)
 
