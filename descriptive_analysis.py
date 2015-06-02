9c0524f4c7b1fbf5ab32df9872af96a1491893e4
thegreatwarlo <carloprovinciali@gmail.com>
2015-06-02 14:45:46 -0700

    added comparison graphs

--- descriptive_analysis.py
+++ descriptive_analysis.py
@@ -50,9 +50,63 @@ for b in businesses:
         if c == 'Food' or c == 'Restaurants':
             food_related.append(b)
             break
+
+non_food = []
+for b in businesses:
+    flag = 0
+    for c in b['categories']:
+        if c == 'Food' or c=='Restaurants':
+            flag = 1
+    if flag == 0:
+        non_food.append(b)
+
+food_bus = set([b['business_id'] for b in food_related])
+nf_bus = set([b['business_id'] for b in non_food])
+
+food_pop = []
+nf_pop = []
+for c in review_count:
+    if c in food_bus:
+        food_pop.append([review_count[c], c])
+    elif c in nf_bus:
+        nf_pop.append([review_count[c], c])
+    else:
+        print "must be error somewhere"
         
+        
+x_to_y = defaultdict(int)
+for c in nf_pop:
+    x_to_y[c[0]] += 1
+
+food_x_to_y = defaultdict(int)
+for c in food_pop:
+    food_x_to_y[c[0]] += 1
     
-    
+X = []
+Y = []
+for x in x_to_y:
+    X.append(x)
+    Y.append(x_to_y[x])
+
+X_food = []
+Y_food = []
+for x in food_x_to_y:
+    X_food.append(x)
+    Y_food.append(food_x_to_y[x])
+
+plt.subplot(211)
+plt.plot(X, Y)
+#plt.title("Number of reviews per business")
+#plt.xlabel("Number of reviews")
+#plt.ylabel("Frequency of occurrence")
+
+plt.subplot(212)
+plt.plot(X_food, Y_food)
+
+plt.plot(X, Y, X_food, Y_food, 'r--')
+
+
+  
 for i in range(50):
     business_id = popularBusinesses[i][1]
     print "category:%s" %category[business_id], "count:%r" % popularBusinesses[i][0] 
@@ -66,12 +120,46 @@ for r in reviews:
 rev_count.sort()
 rev_count.reverse()
 
+nf_rev_count = []
+food_rev_count = []
+for r in reviews:
+    if r['business_id'] in food_bus:
+        food_rev_count.append(len(r['text'].split()))
+    elif r['business_id'] in nf_bus:
+        nf_rev_count.append(len(r['text'].split()))
+    else:
+        print "mamamia"
+
 numpy.median(numpy.array(rev_count))
 
 x_to_y = defaultdict(int)
 for c in rev_count:
     x_to_y[c] += 1
+
+x_to_y = defaultdict(int)
+for c in nf_rev_count:
+    x_to_y[c] += 1
+
+food_x_to_y = defaultdict(int)
+for c in food_rev_count:
+    food_x_to_y[c] += 1
     
+X = []
+Y = []
+for x in x_to_y:
+    X.append(x)
+    Y.append(x_to_y[x])
+
+X_food = []
+Y_food = []
+for x in food_x_to_y:
+    X_food.append(x)
+    Y_food.append(food_x_to_y[x])
+
+plt.plot(X, Y, X_food, Y_food, 'r--')
+plt.title("Number of words per review")
+plt.xlabel("Number of words")
+plt.ylabel("Frequency of occurrence")
 
 X = []
 Y = []
